52a380c80d6c3048b52eeee6355a757e5616d35c
shruti123-cloud <shrutikirtane83@gmail.com>
2024-06-12 17:39:48 +0530

    added

---
+++ src/xyz/Class_001.java
@@ -0,0 +1,13 @@
+package xyz;
+
+public class Class_001 {
+
+	public static void main(String[] args) {
+    System.out.println("hii java");
+    System.out.println("bye java");
+
+		
+		
+	}
+
+}
---
+++ src/xyz/Class_002.java
@@ -0,0 +1,13 @@
+package xyz;
+
+public class Class_002 {
+
+	public static void main(String[] args) {
+
+		
+		int i = 90;
+		System.out.println(i);
+
+	}
+
+}
